bed8d26689414652bf303750d0cf4b837acb26dc
rishabh-397 <rishabh.cha2005@gmail.com>
2025-08-03 21:17:47 +0530

    A. Helpful Maths.cpp

---
+++ A. Helpful Maths.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    string s, ans;
+    cin >> s;
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] != '+') ans.push_back(s[i]);
+    }
+    sort(ans.begin(), ans.end());
+    cout << ans[0];
+    for(int i = 1; i < ans.size(); i++){
+        cout << "+" << ans[i];
+    }
+    return 0;
+}
